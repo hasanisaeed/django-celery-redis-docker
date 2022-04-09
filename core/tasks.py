132c1ce9e0e94c497c82d221a42adfbf6821df0d
Saeed <hassanisaeed19@yahoo.com>
2022-04-09 13:27:02 +0430

    refactor: using @app.task for celery 5

--- core/tasks.py
+++ core/tasks.py
@@ -1,16 +1,18 @@
 from celery.utils.log import get_task_logger
 from time import sleep
 
+from asyncjob.celery import app
+
 logger = get_task_logger(__name__)
 
 
-@task(name='my_first_task')
+@app.task(name='my_first_task')
 def my_first_task(duration):
     sleep(duration)
     return ('first_task_done')
 
 
 # normal function call in python
-my_first_task()
+my_first_task(1)
 # add task to the celery with function call
-my_first_task.delay()
+my_first_task.delay(9)
